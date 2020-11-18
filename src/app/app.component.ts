6bb281bacb28db7c58571aebb8ddff78d6c0a3c1
JosueA44 <71666175+JosueA44@users.noreply.github.com>
2020-11-17 19:10:46 -0600

    Ejemplo sencillo de un CRUD

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Employee } from './models/employee';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'crudSimple';
+  title = 'CRUD Simple';
+  employeArray: Employee[] = [
+    {id: 1, name: 'Josue', country: 'España' },
+    {id: 2, name: 'Karla', country: 'España' },
+    {id: 3, name: 'Elizabeth', country: 'España' },
+    {id: 4, name: 'Marcos', country: 'España' }
+  ];
+  selectedEmployee: Employee = new Employee();
+
+  addOrEdit(){
+    if (this.selectedEmployee.id === 0){
+      this.selectedEmployee.id = this.employeArray.length + 1;
+      this.employeArray.push(this.selectedEmployee);
+    }
+    this.selectedEmployee = new Employee();
+  }
+
+  openEdit(n: Employee){
+    this.selectedEmployee = n;
+  }
+
+  deleted(){
+    if (confirm('Are you sure  ?')){
+      this.employeArray = this.employeArray.filter(x => x != this.selectedEmployee );
+      this.selectedEmployee = new Employee();
+    }
+  }
+
 }
